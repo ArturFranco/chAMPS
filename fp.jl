dcb3a06bd54c86412b16b209592f8069c4b8776d
MarinaCIN <mgb@cin.ufpe.br>
2016-11-01 10:29:35 -0300

    Create fp.jl
    file para criação do fingerprint

---
+++ fp.jl
@@ -0,0 +1,17 @@
+# include("C:\\Users\\mgb\\Downloads\\projeto.jl")
+# Pkg.clone("https://github.com/timotrob/PathLoss.jl.git")
+# Pkg.add("RDatasets")
+# Pkg.add("Distributions")
+# Pkg.add("DataFrames")
+# Pkg.add("DataFramesMeta")
+# Pkg.add("Gadfly")
+using RDatasets
+using Distributions
+using DataFrames
+using DataFramesMeta
+using Gadfly
+using PathLoss
+
+db_train = readtable("C:\\Users\\mgb\\Desktop\\train.csv", separator = ',') #2045 lines
+db_val = readtable("C:\\Users\\mgb\\Desktop\\validation.csv", separator = ',') #257 lines
+db_test = readtable("C:\\Users\\mgb\\Desktop\\test.csv", separator = ',') #257 lines
